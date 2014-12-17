332700279abe34cc4ccfc4e5ce2fff69db84f904
Mimosha <mtmmoei@gmail.com>
2014-12-17 15:43:09 +0700

    TimeDuration@OverallView

--- app/views/PersonView.blade.php
+++ app/views/PersonView.blade.php
@@ -14,6 +14,7 @@
  <script type="text/javascript"
  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDn7BL_KVNfQo3SlE7QCvRZ3xz84CB2T3U">
  </script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
  <script type="text/javascript">
  var tokyo = new google.maps.LatLng(35.66919, 139.7413805);
 
@@ -55,12 +56,40 @@ function initialize() {
 			position: google.maps.ControlPosition.RIGHT_TOP
 		}
 	}
+  
 	swap();
 	document.getElementById('leftPanel').hidden=false; 
 	document.getElementById('addPanel').hidden=true; 
 	map = new google.maps.Map(document.getElementById('map-canvas'),
 		mapOptions);
-	
+	var input = /** @type {HTMLInputElement} */(
+    document.getElementById('inputSearchLocation'));
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+  autocomplete.setTypes([]);
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(12); 
+    }
+    
+    var address = '';
+    if (place.address_components) {
+      address = [
+      (place.address_components[0] && place.address_components[0].short_name || ''),
+      (place.address_components[1] && place.address_components[1].short_name || ''),
+      (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+  });
 }
 
 function prepareData(){
@@ -80,7 +109,20 @@ function drop(){
 		addMarker(i);
 	}
 }
+function switchSearch(){
+  if(searchLocation){
+    searchLocation=false;
+    document.getElementById('searchIcon').setAttribute("class","glyphicon glyphicon-user");
+    document.getElementById('searchLocation').hidden=true;
+    document.getElementById('searchUser').hidden=false;
 
+  }else{
+    searchLocation=true;
+    document.getElementById('searchIcon').setAttribute("class","glyphicon glyphicon-globe");
+    document.getElementById('searchLocation').hidden=false;
+    document.getElementById('searchUser').hidden=true;
+  }
+}
 function cancelUpload(){
 	document.getElementById('leftPanel').hidden=false; 
 	document.getElementById('addPanel').hidden=true; 
@@ -89,14 +131,12 @@ function cancelUpload(){
 function addMarker(iterator) {
 	var tlocation = new google.maps.LatLng(contents[iterator].latitude, contents[iterator].longitude);
 	
-	
 	markers.push(new google.maps.Marker({
 		position: tlocation,
 		map: map,
 		draggable: false,
-		animation: google.maps.Animation.DROP
 	}));
-	var content = location;
+	var content = contents[iterator].date;
 	var marker = markers[iterator];
 	google.maps.event.addListener(marker, 'click', function() {
 		setInfoWindow(content);
@@ -164,8 +204,9 @@ function addData(){
 	document.getElementById('addPanel').hidden=false;
 }
 
-function delUser(){
-
+function cancel(){
+  document.getElementById('leftPanel').hidden=false;  
+  document.getElementById('addPanel').hidden=true;
 }
 // $(function () {
 //   $('[data-toggle="tooltip"]').tooltip()
@@ -175,88 +216,102 @@ function delUser(){
 <body>
 
 	<div id="searchPanel" class="row">
-		<div class="col-sm-2">
-			<a href="{{ URL::to('/') }}" class="btn btn-default" type="button" id="drop"><span class="glyphicon glyphicon-user"></span></a>
-		</div>
-		<div class="input-group col-sm-10">
-			<input type="text" class="form-control" placeholder="Enter a username">
-			<span class="input-group-btn">
-				<button class="btn btn-primary" type="button" id="drop"><span class="glyphicon glyphicon-search"></span></button>
-			</span>
-		</div><!-- /input-group -->
-	</div>
-
-	<div id="leftPanel" class="col-sm-4 col-md-2">
-		<div class="row">
-			<div class="col-sm-6">
-				<h3>{{$person->name}}</h3>
-			</div>
-			<div class="col-sm-6">
-
-				<h3>
-					<button type="button" class="icon" onclick="swap()" data-toggle="tooltip" data-placement="right" title="Switch to Location Log"><span class="glyphicon glyphicon-retweet"></span></button>
-					<button type="button" class="icon" onclick="addData()" data-toggle="tooltip" data-placement="top" title="Add more location information"><span class="glyphicon glyphicon-plus"></span></button>
-					<button type="button" class="icon" data-toggle="modal" data-target="#confirm-delete" data-placement="top" title="Delete this user's location information"><span class="glyphicon glyphicon-trash"></span></button>
-				</h3>
-			</div>
-		</div><!-- /.row -->
-		<h4 id="panelTitle">Predicted Location</h4>
-		<table class="table table-hover" id="locationContents">
-		</table>
-	</div>
+     <div class="col-sm-2">
+      <button class="btn btn-default" type="button" onclick="switchSearch()"><span id="searchIcon" class="glyphicon glyphicon-globe"></span></button>
+    </div>
+		<div class="col-sm-10" id="searchUser" hidden="true">
+      {{ Form::open(array('url' => 'searchUser')) }}
+      <div class="input-group">
+        {{Form::text('username', '', array('class' => 'form-control', 'placeholder' => 'Enter a username'))}}
+        <span class="input-group-btn">
+          {{Form::button( '<span class="glyphicon glyphicon-search"></span>', array('class' => 'btn btn-primary', 'type'=>'submit'))}}
+          <!-- <a href="{{ URL::to('/0') }}" class="btn btn-primary" type="button" id="btnSearchUser" onclick="searchUser()"><span class="glyphicon glyphicon-search"></span></a> -->
+        </span>
+      </div><!-- /input-group -->
+      {{Form::close()}}
+    </div>
+    <div class="col-sm-10" id="searchLocation">
+      <div class="input-group">
+        <input id="inputSearchLocation" type="text" class="form-control" placeholder="Enter a location">
+        <span class="input-group-btn">
+          <button class="btn btn-primary" type="button" id="btnSearchLocation"><span class="glyphicon glyphicon-search"></span></button>
+        </span>
+      </div><!-- /input-group -->
+    </div>
+  </div>
 
 
+  <div id="leftPanel" class="col-sm-4 col-md-2">
+    <div class="row">
+     <div class="col-sm-6">
+      <h3>{{$person->name}}</h3>
+    </div>
+    <div class="col-sm-6">
 
-	<div id="addPanel" class="col-sm-4 col-md-2" hidden="true">
-		<div class="row">
-			<div class="col-sm-6">
-				<h3>{{$person->name}}</h3>
-			</div>
-		</div>
-		<h4 id="panelTitle">Add Location Log</h4>
-		<!-- <form role="form"> -->
-		{{ Form::open(array('url' => 'upload/'.$person->id, 'method' => 'post','files' => true)) }}
-		<div class="form-group">
-			<label for="exampleInputFile">File input</label>
-			{{Form::file('inputFile');}}
-		</div>
-		{{Form::submit('add',array('class'=>'btn btn-primary'))}}
-		<a class="btn btn-default" href="{{ URL::previous()}}">Cancel</a>
-		{{Form::close()}}
-		<!-- </form> -->
-	</div><!--/addPanel -->
-
-	<div id="map-canvas"></div>
+      <h3>
+       <button type="button" class="icon" onclick="swap()" data-toggle="tooltip" data-placement="right" title="Switch to Location Log"><span class="glyphicon glyphicon-retweet"></span></button>
+       <button type="button" class="icon" onclick="addData()" data-toggle="tooltip" data-placement="top" title="Add more location information"><span class="glyphicon glyphicon-plus"></span></button>
+       <button type="button" class="icon" data-toggle="modal" data-target="#confirm-delete" data-placement="top" title="Delete this user's location information"><span class="glyphicon glyphicon-trash"></span></button>
+     </h3>
+   </div>
+ </div><!-- /.row -->
+ <h4 id="panelTitle">Predicted Location</h4>
+ <table class="table table-hover" id="locationContents">
+ </table>
+</div>
 
 
 
-	<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-sm">
-			<div class="modal-content">
+<div id="addPanel" class="col-sm-4 col-md-2" hidden="true">
+  <div class="row">
+   <div class="col-sm-6">
+    <h3>{{$person->name}}</h3>
+  </div>
+</div>
+<h4 id="panelTitle">Add Location Log</h4>
+<!-- <form role="form"> -->
+{{ Form::open(array('url' => 'upload/'.$person->id, 'method' => 'post','files' => true)) }}
+<div class="form-group">
+ <label for="exampleInputFile">File input</label>
+ {{Form::file('inputFile');}}
+</div>
+{{Form::submit('Add',array('class'=>'btn btn-primary'))}}
+<a class="btn btn-default" onclick="cancel()">Cancel</a>
+{{Form::close()}}
+<!-- </form> -->
+</div><!--/addPanel -->
 
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					<h4 class="modal-title" id="myModalLabel">Confirm Delete</h4>
-				</div>
+<div id="map-canvas"></div>
 
-				<div class="modal-body">
-					<p>Are you sure you want to delete this user?</p>
-					<p class="debug-url"></p>
-				</div>
 
-				<div class="modal-footer">
-					
-          {{ Form::open(array('url' => '/deleteUser')) }}
-          {{ Form::hidden('id', $person->id) }}
-          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-          {{ Form::submit('Yes',array('class'=>'btn btn-danger'))}}
-          {{ Form::close() }}
-				</div>
-			</div>
-		</div>
-	</div>
-	
-	<!-- {{HTML::script('js/bootstrap.min.js');}} -->
+
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+   <div class="modal-content">
+
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+     <h4 class="modal-title" id="myModalLabel">Confirm Delete</h4>
+   </div>
+
+   <div class="modal-body">
+     <p>Are you sure you want to delete this user?</p>
+     <p class="debug-url"></p>
+   </div>
+
+   <div class="modal-footer">
+
+    {{ Form::open(array('url' => '/deleteUser')) }}
+    {{ Form::hidden('id', $person->id) }}
+    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+    {{ Form::submit('Yes',array('class'=>'btn btn-danger'))}}
+    {{ Form::close() }}
+  </div>
+</div>
+</div>
+</div>
+
+<!-- {{HTML::script('js/bootstrap.min.js');}} -->
  <!-- {{HTML::script('js/jquery-1.11.1.min.js');}}
  {{HTML::script('js/jquery.js');}} -->
  <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
