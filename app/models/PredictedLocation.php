ed3411f88abc4f062c16295195e9b1299c867734
Panida <cp.panida@gmailcom>
2014-12-17 16:32:41 +0700

    finish prediction

--- app/models/PredictedLocation.php
+++ app/models/PredictedLocation.php
@@ -15,12 +15,16 @@ class PredictedLocation extends Eloquent{
 	}
 
 	public static function storePredictedData($predictedLocationList, $personId, $lastestDate){
-		var_dump($lastestDate);
-		$tempDateTime = Carbon::createFromFormat('Y-m-d H:m:i', $lastestDate);
+		//var_dump($lastestDate);
+		//$tempDateTime = Carbon::createFromFormat('Y-m-d H:m:i', $lastestDate);
+		
+
 		foreach ($predictedLocationList as $predictedLocation) {
 			$location = new PredictedLocation;
 			$location->latitude = $predictedLocation->lat;
 			$location->longitude = $predictedLocation->lng;
+			
+			$tempDateTime = Carbon::create(date_format(new DateTime($lastestDate), 'Y'), date_format(new DateTime($lastestDate), 'm'), date_format(new DateTime($lastestDate), 'd'), date_format(new DateTime($lastestDate), 'H'), date_format(new DateTime($lastestDate), 'i'), date_format(new DateTime($lastestDate), 's'));
 			$newDateTime = $tempDateTime->addHours(intval($predictedLocation->time));
 			$location->dateTime = new DateTime($newDateTime->toDateTimeString());
 			$location->personId = $personId;
