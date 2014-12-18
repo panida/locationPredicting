05473aa38f04849cdc6a3bf93827ce14308727ef
Panida <cp.panida@gmailcom>
2014-12-18 15:53:58 +0700

    edit view to show all user [have bug cannot click to show marker]

--- app/views/OverallView.blade.php
+++ app/views/OverallView.blade.php
@@ -265,42 +265,26 @@ function clearMarkers() {
 //   }
 // }
 function prepareContentHTML(){
-  var text="'<tbody>'";
-  if(contents.length!=0){
-  // dateTime = new Date();
-  // var time=dateTime.getTime();
-  if(contents[0].length!=0){
-    text ='<tr>'+
-    '<td onclick="showMarkers()">All</td>'+
-    '</tr>';
- //  console.log(contents[0].length);
- //  for(var i=0;i<24;i++){
- //   text += '<tr>';
- //   text += '<td onclick="showSpecificMarkers('+i+')">';
- //   text += ''+dateTime.getFullYear()+'-'+dateTime.getMonth()+'-'+dateTime.getDate();
- //   text += ' '+addZero(dateTime.getHours())+':'+addZero(dateTime.getMinutes())+':'+addZero(dateTime.getSeconds())+'</td>';
- //   text += '</tr>';
- //   dateTime.setHours(dateTime.getHours()+1);
- // }
-
- console.log(contents[0].length);
- for(var i=0;i<contents[0].length;i++){
-   text += '<tr>';
-   text += '<td onclick="showSpecificMarkers('+i+')">';
-   text += ''+contents[0][i].date+'</td>';
-   text += '</tr>';
- }
-}
-else{
- text ='<tr>'+
- '<td">No predicted location</td>'+
- '</tr>';
-}
+  var text='<tbody>';
+  for(var i=0; i<contents.length;i++){
+    text +='<tr>'+'<td><h4>'+users[i]+'</h4></td>'+'</tr>';
+    for(var j=0;j<contents[i].length;j++){
+      text += '<tr>';
+      text += '<td onclick="showSpecificMarkers('+j+')">';
+      text += ''+contents[i][j].date+'</td>';
+      text += '</tr>';
+    }
+    if(contents[i].length==0){
+      text +='<tr>'+'<td>No predicted location</td>'+'</tr>';
+    }
+  }
+  
 
 
-text += '</tbody>';
-}
-return text;
+
+  text += '</tbody>';
+
+  return text;
 }
 // function addZero(i) {
 //     if (i < 10) {
